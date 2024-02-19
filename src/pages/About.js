93618a9d6f015f301733af65f35a1cfa098411e1
pawan-7218 <pawanzade558@gmail.com>
2024-02-19 17:12:41 +0530

    about page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,27 @@
 import React from 'react'
-
+import Header from '../components/Header-Homepage'
+import styles from '../components/MyComponent.module.css'
+import mario from '../Assests/mario.jpg'
+import B from '../Assests/B.jpg'
+import './about.css'
 const About = () => {
   return (
-    <div className="about-section">
-    <div className="brand-info">
-      <h1>Brand Name</h1>
-      <p>Location: Your Location</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div className={styles.aboutsection}>
+      <Header/>
+      <main>
+      <div className={styles.brandinfo}>
+      <h1>Little Lemon</h1>
+      <p>Location:Chicago</p>
+      <p className={styles.ab}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
+      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>
+      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
-    <div className="owners-images">
-      <img src="owner1.jpg" alt="Owner 1" className="owner-image" />
-      <img src="owner2.jpg" alt="Owner 2" className="owner-image" />
+    <div className="ownersimages">
+      <div className='owner1'><img src={mario} alt="Owner 1" className="ownerimage" /></div>
+      <div className='owner2'><img src={B} alt="Owner 2" className="ownerimage" /></div>
     </div>
+      </main>
+    
   </div>
   )
 }
